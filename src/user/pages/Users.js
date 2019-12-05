ff09d86da46c845f3ebfc4c9d84e6cbd56b888b8
art1373 <alirezatk73@gmail.com>
2019-12-05 12:30:03 +0330

    added forms for add place and update place using hooks, added validation

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import UsersList from "../components/UsersList";
+import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
     {
-      id: "u1",
-      name: "Max Schwarz",
-      image: "https://avatars0.githubusercontent.com/u/28806196?s=460&v=4",
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
       places: 3
     }
   ];
